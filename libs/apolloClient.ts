8411494c377cbcfe611e5378644350ff20c5d6ce
Dheeraj <137279485+dheerajt112@users.noreply.github.com>
2024-06-09 17:19:05 +0530

    updated ngrok link

--- libs/apolloClient.ts
+++ libs/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri:'http://localhost:3008/graphql',
+    uri:'https://d37b-2401-4900-22da-d27d-e0e6-f5a7-e6c4-e577.ngrok-free.app/graphql',
     cache: new InMemoryCache(),
     credentials:"include"
 });
